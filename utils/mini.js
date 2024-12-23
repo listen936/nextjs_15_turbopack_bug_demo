b795bf0a572ae3198d0444b7aa6f89c2d5f52c6f
leanwang <leanwang@zego.im>
2024-12-23 11:02:09 +0800

    init

--- utils/mini.js
+++ utils/mini.js
@@ -1,5 +1,5 @@
 // namespace builder
-(function _(namespace, path) {
+(function _(namespace, path, parent) {
   // The expected output is "", "wrapper", "wrapper.data", "wrapper.data.key"
   // The actual output is "", "wrapper", "data", "key"
   console.debug("####", path)
@@ -7,7 +7,7 @@
   for (var i = 0, keys = Object.keys(namespace); i < keys.length; i++) {
     var key = keys[i];
     // --------------------------------------------------------------------------
-    console.debug("####", path ? true : false) // always true? why?
+    console.debug("####", path, path ? true : false) // always true? why?
     // --------------------------------------------------------------------------
     parent[key] = _(namespace[key], path ? `${path}.${key}` : key, parent[key] || {});
     // --- If the line below is replaced, it will meet the expectations.
@@ -20,5 +20,5 @@
       key: {}
     },
   }
-}, '');
+}, '', {});
 
